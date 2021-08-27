eee446a4016feb1ff56091a842d84f5a4b4cc39f
cnting <1040594303@qq.com>
2021-08-27 14:10:29 +0800

    big img

--- Bitmap/app/src/main/java/com/cnting/bitmap/BitmapUtil.kt
+++ Bitmap/app/src/main/java/com/cnting/bitmap/BitmapUtil.kt
@@ -1,13 +1,14 @@
 package com.cnting.bitmap
 
 import android.graphics.Bitmap
+import android.util.Log
 
 /**
  * Created by cnting on 2021/8/23
  *
  */
 object BitmapUtil {
-    fun getBitmapSize(width:Int,height:Int,config: Bitmap.Config): Int {
+    fun getBitmapSize(width: Int, height: Int, config: Bitmap.Config): Int {
         return width * height * getByteFromConfig(config)
     }
 
@@ -19,5 +20,26 @@ object BitmapUtil {
             Bitmap.Config.ARGB_8888 -> 4
             else -> 0
         }
+    }
+
+    fun computeSampleSize(
+        originWidth: Int,
+        originHeight: Int,
+        targetWidth: Int,
+        targetHeight: Int
+    ): Int {
+        var sampleSize = 1
+        var finalWidth = originWidth
+        var finalHeight = originHeight
+        while (finalWidth > targetWidth || finalHeight > targetHeight) {
+            sampleSize *= 2
+            finalWidth /= 2
+            finalHeight /= 2
+            Log.i(
+                "===>",
+                "finalWidth:$finalWidth,finalHeight:$finalHeight,targetWidth:$targetWidth,targetHeight:$targetHeight"
+            )
+        }
+        return sampleSize
     }
 }
--- Bitmap/app/src/main/java/com/cnting/bitmap/MainActivity.kt
+++ Bitmap/app/src/main/java/com/cnting/bitmap/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.cnting.bitmap
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,19 +10,10 @@ class MainActivity : AppCompatActivity() {
         loadBitmap()
     }
 
+
     private fun loadBitmap() {
-        val options = BitmapFactory.Options()
-        options.inJustDecodeBounds = true
-
-        BitmapFactory.decodeResource(resources, R.drawable.a, options)
-
-        Log.d("===>", "targetDensity:${options.inTargetDensity}，inDensity:${options.inDensity}")
-
-        Log.d("===>", "图片宽:${options.outWidth},高:${options.outHeight},占用内存:${BitmapUtil.getBitmapSize(options.outWidth, options.outHeight, options.inPreferredConfig)}")
-//        options.inPreferredConfig = Bitmap.Config.RGB_565
-        options.inJustDecodeBounds = false
-        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.a, options)
-        Log.d("===>", "图片宽:${bitmap.width},高:${bitmap.height},占用内存:${BitmapUtil.getBitmapSize(bitmap.width, bitmap.height, bitmap.config)}")
-
+        val assetManager = resources.assets
+        val inputStream = assetManager.open("long.jpeg")
+        findViewById<BigImageView>(R.id.img).setFilePath(inputStream)
     }
 }
